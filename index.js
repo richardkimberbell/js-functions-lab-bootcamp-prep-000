bae30cd2939cb2ac212b4b394493b200563b8eed
Richard Kimber-Bell <rickykimberbell@gmail.com>
2019-04-20 16:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return "Happy holidays, "+name+"!"
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday,name) {
-  return "Happ"
+  return `Happy ${holiday}, ${name}!`
 }
